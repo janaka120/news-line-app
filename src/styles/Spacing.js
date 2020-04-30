e05253b8b227a52ba6c3264f118b8d423ab38826
janaka <janaka@m-daq.com>
2020-04-30 12:05:14 +0530

    Add styles

---
+++ src/styles/Spacing.js
@@ -0,0 +1,7 @@
+// @flow
+import {scaleSize} from './Mixins';
+
+export const SCALE_18 = scaleSize(18);
+export const SCALE_16 = scaleSize(16);
+export const SCALE_12 = scaleSize(12);
+export const SCALE_8 = scaleSize(8);
